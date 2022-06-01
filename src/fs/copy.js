69a9fc3bd591780a8b5989120a0e6ecc4392b6b4
Andrei Krasko <andrei@krasko.by>
2022-06-01 13:10:58 +0300

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+import fsPromises from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const files = await fsPromises.readdir(path.resolve(__dirname, 'files'), { withFileTypes: true });
+    //const rmdir = await fsPromises.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true });
+    const mkdir = await fsPromises.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
+    for (const file of files) {
+        if (file.isFile()) {
+            const copy = await fsPromises.copyFile(path.resolve(__dirname, 'files', file.name), path.resolve(__dirname, 'files-copy', file.name));
+        }
+    }
 };
+copy();
