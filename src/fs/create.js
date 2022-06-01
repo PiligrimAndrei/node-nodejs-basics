c2b3953632b51c67f23f88c248dfe03d1e87332a
Andrei Krasko <andrei@krasko.by>
2022-06-01 12:11:22 +0300

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+
 export const create = async () => {
-    // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    fs.access(path.resolve(__dirname, 'files', 'fresh.txt'), (err) => {
+        if (err) {
+            throw Error;
+        } else {
+            const writableStream = fs.createWriteStream(path.resolve(__dirname, 'files', 'fresh.txt'))
+            console.log(path.resolve(__dirname, 'files', 'fresh.txt'))
+            writableStream.write('I am fresh and young');
+        }
+    })
+}
+create();
