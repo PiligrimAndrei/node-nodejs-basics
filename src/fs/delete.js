fd18876d1650e406841e9799d11de2f4ea00d932
Andrei Krasko <andrei@krasko.by>
2022-06-02 10:11:27 +0300

    feat: add delete.js OK

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,26 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+
 export const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePass = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+    fs.stat(filePass, (err) => {
+        if (err) {
+            console.log('fileToRemove.txt no file');
+            throw new Error('FS operation failed');
+        } else {
+            fs.unlink(filePass, (err) => {
+                if (err) {
+                    console.log(err)
+                } else console.log('File remove')
+            })
+        };
+    })
 };
+
+remove();
+//node src/fs/delete.js
