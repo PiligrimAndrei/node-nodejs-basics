601d219432b96710654e284b304db4d198171994
Andrei Krasko <andrei@krasko.by>
2022-06-02 13:14:08 +0300

    feat: add env.js OK

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const arr = process.env;
+    const keys = Object.keys(arr);
+    //console.log(keys);
+    for (const key of keys) {
+        if (key.includes('RSS_')) {
+            console.log(key, '=', arr[key])
+        }
+    }
 };
+parseEnv()
