8b8cd4b1951c6c3634078fa76b1fbf3f37b99463
Andrei Krasko <andrei@krasko.by>
2022-06-03 11:41:29 +0300

    feat: add calcHash.js OK

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,25 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+import fsPromises from 'fs/promises';
+import crypto from 'crypto';
+import { hasUncaughtExceptionCaptureCallback } from 'process';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const hash = crypto.createHash('SHA256');
+    const filePass = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const file = fs.createReadStream(filePass);
+    hash.setEncoding('hex');
+
+    file.on('end', () => {
+        hash.end();
+        console.log(hash.read());
+    })
+    file.pipe(hash)
 };
+calculateHash()
+// node src/hash/calcHash.js
