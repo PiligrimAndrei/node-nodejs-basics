d9afd09b83acc0c315a7c7c0bbc4f07d6dffd82b
Andrei Krasko <andrei@krasko.by>
2022-06-03 11:26:47 +0300

    feat: add args.js OK

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
-};
+    const myArgs = process.argv;
+
+    for (let i = 0; i < myArgs.length - 1; i++) {
+        if (myArgs[i].includes('--')) {
+            console.log(myArgs[i].slice(2), ' is ', myArgs[i + 1])
+        }
+    }
+    ;
+}
+parseArgs();
+
+//node src/cli/args.js --propName value --prop2Name value2 --prop3Name value3 --test 55555
