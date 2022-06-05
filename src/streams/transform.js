836d806e5196c64fc2e7304c62415ee8381f410e
Andrei Krasko <andrei@krasko.by>
2022-06-05 19:50:16 +0300

    feat: add tranform.js OK

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,33 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+import { pipeline, Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+
+    const readable = process.stdin;
+    const writable = process.stdout;
+
+    const transformStream = new Transform({
+        transform(chunk, enk, cb) {
+            const chunkString = chunk.toString().trim();
+            const chunkReverse = chunkString.split('').reverse().join('');
+            this.push(chunkReverse + '\n');
+
+            cb()
+        }
+    });
+
+    pipeline(
+        readable,
+        transformStream,
+        writable,
+        err => {
+            console.log('Error:', err)
+        }
+    )
 };
+
+transform();
+//node src/streams/transfrom.js
